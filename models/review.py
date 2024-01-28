60b4f273baf8c42f067189c319f0918dec741678
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 10:28:33 -0500

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 """This module creates a Review class"""
 
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base, Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
-class Review(BaseModel):
-    """Class for managing review objects"""
+class Review(BaseModel, Base):
+    """This is the class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = 'reviews'
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        user = relationship(
+            'User', back_populates='reviews')  # cascade? slave
+        place = relationship(
+            'Place', back_populates='reviews')  # cascade? slave
+
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
