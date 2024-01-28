80a04c643244d4a21604779488b9c1df7ef86801
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 10:27:32 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module creates a User class"""
 
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base, Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """Class for managing city objects"""
+class City(BaseModel, Base):
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
 
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship(
+            'State', back_populates='cities')
+    else:
+        name = ""
+        state_id = ""
